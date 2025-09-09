b96f9e7348ed37f3e4d2a4caf2428f113e37db45
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-09 11:52:31 -0300

    exercise 04: input two numbers and calculate subtraction

---
+++ programming_learn_beginner/exercise04.c
@@ -0,0 +1,28 @@
+/*
+Author: Francisco Gomes da Silva Junior
+Date: 09/09/2025 - in format Brazil
+Description: Read a two numbers, calculate sum numbers and printing value final
+version: 1.0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // Section of variables
+    int number_one, number_two, subtraction;
+
+    // Read of user input data
+    printf("The number one is: ");
+    scanf("%d", &number_one);
+
+    printf("The number two is: ");
+    scanf("%d", &number_two);
+
+    subtraction = number_one - number_two; // variable created in section head
+
+    printf("The subtraction of two numbers is: %d\n", subtraction);                                // first format for printing value, using variable
+    printf("The second format for printing value of subtraction is: %d", number_one - number_two); // Calculating directly in function "printf"
+
+    //
+}
