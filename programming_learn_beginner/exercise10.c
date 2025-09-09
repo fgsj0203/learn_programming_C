355cd4f8ad7327d94ce8b112baa1006c826cc88f
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-09 16:05:16 -0300

    exercise 10: read and printing value of amount apple

---
+++ programming_learn_beginner/exercise10.c
@@ -0,0 +1,21 @@
+/*
+Author: Francisco Gomes da Silva Junior
+Date: 09/09/2025 - in format Brazil
+Description: Read amount of apple and printing value
+version: 1.0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // section variables
+    int amount_apple;
+
+    // Read data user input
+    printf("What amount of apple: ");
+    scanf("%d", &amount_apple);
+
+    // Printing value
+    printf("The amount of apple is: %d", amount_apple);
+}
