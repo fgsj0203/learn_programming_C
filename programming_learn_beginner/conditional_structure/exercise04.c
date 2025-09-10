456a7db733339eee6d9196504ff5801668dc3f10
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-10 20:10:31 -0300

    exercise 04: read a two numbers and identify what bigger

---
+++ programming_learn_beginner/conditional_structure/exercise04.c
@@ -0,0 +1,31 @@
+/*
+Author: Francisco Gomes da Silva Junior
+Date: 09/09/2025 - in format Brazil
+Description: read two number is identify a bigger number
+version: 1.0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // Section variables
+    int number_one, number_two;
+
+    // Reading a data user input
+    printf("Number one: ");
+    scanf("%d", &number_one);
+
+    printf("Number two: ");
+    scanf("%d", &number_two);
+
+    // Conditional structure
+    if (number_one > number_two)
+    {
+        printf("The number one - %d > number two - %d", number_one, number_two);
+    }
+    else
+    {
+        printf("The number two - %d > number one - %d", number_two, number_one);
+    }
+}
