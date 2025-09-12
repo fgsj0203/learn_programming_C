1b4e68f569bb36bb37d1827c068fd1ba3973bbe7
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-12 20:52:26 -0300

    exercise 07

---
+++ programming_learn_beginner/conditional_structure/exercise07.c
@@ -0,0 +1,29 @@
+/*
+Author: Francisco Gomes da Silva Junior
+Date: 12/09/2025 - in format Brazil
+Description: Read note of test student and identify status
+version: 1.0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // Section of variables
+    float note;
+
+    // Read data user input
+    printf("Note student: ");
+    scanf("%f", &note);
+
+    // Conditional structure
+    if (note > 7)
+    {
+        printf("Approved");
+    }
+    else
+    {
+        printf("Reproved");
+    }
+    return 0;
+}
