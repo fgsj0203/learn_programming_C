dd83dc7f367a3ef70e212aff95bfdd358d34ed94
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-26 10:47:05 -0300

    exercise 06

---
+++ programming_learn_beginner/conditional_structure_intermediary/exercise06.c
@@ -0,0 +1,41 @@
+/*
+Author: Francisco Gomes da Silva Junior
+Date: 26/09/2025 - in format Brazil
+Description: read three notes, calculate average and identified if average is approved, in recuperation or repproved.
+version: 1.0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // Section of variables
+    float note_one, note_two, note_three, average_notes;
+
+    // Read data user input
+    printf("Note one: ");
+    scanf("%f", &note_one);
+
+    printf("Note two: ");
+    scanf("%f", &note_two);
+
+    printf("Note three: ");
+    scanf("%f", &note_three);
+
+    // calculate average of notes
+    average_notes = (note_one + note_two + note_three) / 3.0;
+
+    // Validation status of student
+    if (average_notes >= 7.0 && average_notes <= 10.0)
+    {
+        printf("You is approved. The note is: %.2f\n", average_notes);
+    }
+    else if (average_notes >= 5.0 && average_notes <= 6.9)
+    {
+        printf("You is recuperation. The note is: %.2f\n", average_notes);
+    }
+    else
+    {
+        printf("You is repproved. The note is: %.2f\n", average_notes);
+    }
+}
